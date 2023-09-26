10ea8ab27c24e39204b230399b1a4791bfa72482
veronicajujuy <veronica.jujuy@gmail.com>
2023-09-26 17:36:37 -0300

    agregando detalle y getOneMovie

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,11 +1,17 @@
 const db = require("../database/models");
 const mainController = {
   getAllMovies: (req, res) => {
-    db.Movie.findAll()
+    db.Movie.findAll({ raw: true })
       .then((result) => {
         res.render("getAll", { movies: result });
       })
-      .catch((error) => console.log(res.send(error)));
+      .catch((error) => res.send(error));
+  },
+  getOneMovie: (req, res) => {
+    const id = req.params.id;
+    db.Movie.findByPk(id, { raw: true })
+      .then((result) => res.render("getOne", { movie: result }))
+      .catch((error) => res.send(error));
   },
 };
 
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -3,5 +3,6 @@ const mainController = require("../controllers/mainController");
 const mainRouter = express.Router();
 
 mainRouter.get("/", mainController.getAllMovies);
+mainRouter.get("/movies/:id", mainController.getOneMovie);
 
 module.exports = mainRouter;
